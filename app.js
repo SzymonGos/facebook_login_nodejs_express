27018b92c87355624d71210c963dc55baf955f60
Szymon Gos <szymonngos@gmail.com>
2021-08-13 13:10:19 +0100

    import modules / setup passport for authentication with Facebook

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// initial commit
+let express = require('express');
+
+//bodyParser: node.js middleware for handling incoming URL requests
+let bodyParser = require('body-parser');
+let app = express();
+let passport = require('passport');
+let FacebookStrategy = require('passport-facebook').Strategy
+let cookieParser = require('cokie-parser');
+let session = require('express-session');
+
+// app.use: register chain of middlewares before executing any end route logic
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'heyboard cat', key: 'sid' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Passport session setup
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
